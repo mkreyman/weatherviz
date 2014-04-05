a40c351ad728fdbb4c1a6fead9adcd63edf7b1aa
Mark Kreyman <mark@kreyman.com>
2014-04-04 20:27:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherViz::Application.config.secret_key_base = '7532358c852b268ee63ce6888340154b3ea51660751634f5822124672a3aabbba34b962db651abba6827e6705a711128abb0152149891404771fc2eeb0476dda'
