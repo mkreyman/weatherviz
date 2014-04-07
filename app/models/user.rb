1e8666744f7ab1c8d39a3a159aa24c9b0d9265c9
Mark Kreyman <mark@kreyman.com>
2014-04-07 16:40:29 -0600

    Added auth with GitHub account
    [#69034116]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password(validations: false)
+
+  validates_confirmation_of :password,
+    if: lambda { |user| !user.omniauth? && user.password.present? }
+  validates_presence_of :password, :on => :create,
+    if: lambda { |user| !user.omniauth? }
+  validates_presence_of :password_confirmation, :on => :create,
+    if: lambda { |user| !user.omniauth? && user.password.present? }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407173601) do
+ActiveRecord::Schema.define(version: 20140407213904) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140407173601) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "omniauth"
   end
 
 end
