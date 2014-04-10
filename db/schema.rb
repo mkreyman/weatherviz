b5bbd0b80c0c6fb5b3722ff10c258a0d1a7d0aec
Mark Kreyman <mark@kreyman.com>
2014-04-10 16:29:07 -0600

    Cleaned up WeatherFetcher class and removed its table
    [#68586306]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409175740) do
+ActiveRecord::Schema.define(version: 20140409041325) do
 
   create_table "locations", force: true do |t|
     t.integer  "city_id"
@@ -32,12 +32,6 @@ ActiveRecord::Schema.define(version: 20140409175740) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "omniauth"
-  end
-
-  create_table "weather_fetchers", force: true do |t|
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "weather_reports", force: true do |t|
