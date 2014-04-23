247753829762b1ba5d8b2bb2f8d2a863fcd3ca22
Mark Kreyman <mark@kreyman.com>
2014-04-22 21:19:24 -0600

    Setting up redis for heroku attempt 13

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,3 +1,4 @@
 redis_url = ENV['REDISTOGO_URL'] || "redis://localhost:6379/0/weatherviz"
+$redis = Redis.new(:url => redis_url)
 WeatherViz::Application.config.cache_store = :redis_store, redis_url
 WeatherViz::Application.config.action_dispatch.rack_cache = :redis_store, redis_url
