c071609ad00371a905e1ed0250bc404f3b1db9ba
Mark Kreyman <mark@kreyman.com>
2014-04-29 17:56:21 -0600

    Added refactored -visitor partial

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
+  @location = Location.new
+
+  def visitor_location
+    if Rails.env.test? || Rails.env.development?
+      @response = Geocoder.search('174.51.246.225').first
+    else
+      @response ||= request.location
+    end
+    @location = Location.where(city: @response.city,
+                               state_code: @response.state_code)
+    .first_or_create do |location|
+      location.city = @response.city
+      location.state_code = @response.state_code
+      location.latitude = @response.latitude
+      location.longitude = @response.longitude
+    end
+  end
 end
