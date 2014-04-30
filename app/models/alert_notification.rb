4a907c3cceb40610251c1eb035b00c7ff01dc1aa
Mark Kreyman <mark@kreyman.com>
2014-04-30 12:54:46 -0600

    Defined #send_alert

--- app/models/alert_notification.rb
+++ app/models/alert_notification.rb
@@ -1,22 +1,26 @@
 class AlertNotification
 
   def send_alert(alert, weather_report)
-    @alert_messages = alert.messages(weather_report)
+    alert_messages = alert.messages(weather_report)
     alert_types = alert.alert_types
-    if @alert_messages.any?
-      AlertLog.create(sent_at: Time.zone.now, alert_message: @alert_messages.join("\n"), alert_message_types: alert_types.join(" "))
-      send_sms(alert_types[:sms])
-      send_email(alert_types[:email])
+    message = "#{alert.alert_name}: #{alert_messages.join("\n")}"
+    if alert_messages.any?
+      AlertLog.create(alerts_id: alert.id,
+                      sent_at: Time.zone.now,
+                      alert_message: alert_messages.join("\n"),
+                      alert_message_types: alert_types.join(" "))
+      send_sms(alert_types[:sms], message)
+      send_email(alert_types[:email], message)
     end
   end
 
-  def send_sms(sms_number)
+  def send_sms(sms_number, message)
     if sms_number
       # Send SMS to sms_number
     end
   end
 
-  def send_email(email)
+  def send_email(email, message)
     if email
       # Send email to email address
     end
