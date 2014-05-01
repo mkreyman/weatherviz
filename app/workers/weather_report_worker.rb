628f53d352d173002961f5abc2875515e9e40c81
Mark Kreyman <mark@kreyman.com>
2014-05-01 03:13:48 -0600

    Added weather_report_worker back

---
+++ app/workers/weather_report_worker.rb
@@ -0,0 +1,13 @@
+class WeatherReportWorker
+  @queue = :weather_report_worker
+
+  def self.perform
+    Location.where(id: Alert.select("distinct(location_id)")
+                       .map(&:location_id)).each do |location|
+      weather_report = WeatherFetcher.fetch(location)
+      location.alerts.map do |alert|
+        AlertNotification.send_alert(alert, weather_report)
+      end
+    end
+  end
+end
