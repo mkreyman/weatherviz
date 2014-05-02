8f133e66ad7e837dd5aa69db5315a7c6f5795e2c
Mark Kreyman <mark@kreyman.com>
2014-05-01 23:34:21 -0600

    WIP

--- app/workers/location_fetcher_worker.rb
+++
@@ -1,7 +0,0 @@
-class LocationFetcherWorker
-  @queue = :location_fetcher_worker
-
-  def self.perform(search)
-    LocationFetcher.fetch(search)
-  end
-end
--- app/workers/weather_fetcher_worker.rb
+++
@@ -1,7 +0,0 @@
-class WeatherFetcherWorker
-  @queue = :weather_fetcher_worker
-
-  def self.perform(location)
-    WeatherFetcher.fetch(location)
-  end
-end
